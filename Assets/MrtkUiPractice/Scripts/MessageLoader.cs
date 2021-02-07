59562ff9eb20e68a9333ca997b5056012271e7a4
shuhei.nishizawa <shuhei.nishizawa@avanade.com>
2021-02-07 19:10:50 +0900

    Change text when push button

---
+++ Assets/MrtkUiPractice/Scripts/MessageLoader.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MessageLoader : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+
+    public TMPro.TextMeshPro Message;
+
+    /// <summary>
+    /// Load message from file in Resources folder
+    /// </summary>
+    public void LoadMessage()
+    {
+        TextAsset asset = Resources.Load<TextAsset>("Message");
+        Debug.Log(asset.text);
+
+        Message.text = asset.text;
+        Message.color = Color.red;
+    }
+}
