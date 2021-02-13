722f840ff7f2cd53494140baf51cec94799f4f6b
shuhei.nishizawa <shuhei.nishizawa@avanade.com>
2021-02-13 21:10:49 +0900

    Add item to ScrollingObjectCollection when Add button pushed

---
+++ Assets/MrtkUiPractice/Scripts/MyScrollViewItemManager.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.MixedReality.Toolkit.UI;
+using Microsoft.MixedReality.Toolkit.Utilities;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MyScrollViewItemManager : MonoBehaviour
+{
+    public GameObject Prefab;
+    public GameObject TargetCollection;
+    public GameObject TargetScrollingObjectCollectionObject;
+
+    private ScrollingObjectCollection scrollingObjectCollectionComponent;
+    private GridObjectCollection gridObjectCollectionComponent;
+
+    public void AddNewItem()
+    {
+        //get ScrollingObjectCollection componet
+        scrollingObjectCollectionComponent = TargetScrollingObjectCollectionObject.GetComponent<ScrollingObjectCollection>();
+
+        //get GridObjectCollection componet from children of ScrollingObjectCollection component
+        gridObjectCollectionComponent = scrollingObjectCollectionComponent.GetComponentInChildren<GridObjectCollection>();
+
+        //Create new item into GridObjectCollection component
+        GameObject itemInstance = Instantiate(Prefab, gridObjectCollectionComponent.transform);
+        itemInstance.SetActive(true);
+
+        gridObjectCollectionComponent.UpdateCollection();
+        scrollingObjectCollectionComponent.UpdateContent();
+    }
+}
---
+++ Assets/MrtkUiPractice/Scripts/MyScrollablePagination.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.MixedReality.Toolkit.UI;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MyScrollablePagination : MonoBehaviour
+{
+    /// <summary>
+    /// Example script of how to navigate a <see cref="Microsoft.MixedReality.Toolkit.UI.ScrollingObjectCollection"/> by pagination.
+    /// Allows the call to scroll pagination methods from the inspector.
+    /// </summary>
+    [SerializeField]
+    [Tooltip("The ScrollingObjectCollection to navigate")]
+    private ScrollingObjectCollection scrollView;
+    public ScrollingObjectCollection ScrollView
+    {
+        get
+        {
+            if (scrollView == null)
+            {
+                scrollView = GetComponent<ScrollingObjectCollection>();
+            }
+            return scrollView;
+        }
+        set
+        {
+            scrollView = value;
+        }
+    }
+
+    /// <summary>
+    /// Smoothly moves the scroll container a relative number of tiers of cells.
+    /// </summary>
+    public void ScrollByTier(int amount)
+    {
+        Debug.Assert(ScrollView != null, "Scroll view needs to be defined before using pagination.");
+        scrollView.MoveByTiers(amount);
+    }
+}
