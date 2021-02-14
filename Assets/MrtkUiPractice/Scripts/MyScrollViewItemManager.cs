88793b0c766fb198e4bffa98120a303e0c58d2a3
shuhei.nishizawa <shuhei.nishizawa@avanade.com>
2021-02-14 12:10:46 +0900

    Add Remove item function to ScrollObjectCollection

--- Assets/MrtkUiPractice/Scripts/MyScrollViewItemManager.cs
+++ Assets/MrtkUiPractice/Scripts/MyScrollViewItemManager.cs
@@ -7,23 +7,40 @@ using UnityEngine;
 public class MyScrollViewItemManager : MonoBehaviour
 {
     public GameObject Prefab;
-    public GameObject TargetCollection;
-    public GameObject TargetScrollingObjectCollectionObject;
 
     private ScrollingObjectCollection scrollingObjectCollectionComponent;
     private GridObjectCollection gridObjectCollectionComponent;
 
-    public void AddNewItem()
+    private void Start()
     {
         //get ScrollingObjectCollection componet
-        scrollingObjectCollectionComponent = TargetScrollingObjectCollectionObject.GetComponent<ScrollingObjectCollection>();
-
+        scrollingObjectCollectionComponent = GetComponent<ScrollingObjectCollection>();
         //get GridObjectCollection componet from children of ScrollingObjectCollection component
         gridObjectCollectionComponent = scrollingObjectCollectionComponent.GetComponentInChildren<GridObjectCollection>();
+    }
 
+    public void AddNewItem()
+    {
         //Create new item into GridObjectCollection component
         GameObject itemInstance = Instantiate(Prefab, gridObjectCollectionComponent.transform);
         itemInstance.SetActive(true);
+
+        gridObjectCollectionComponent.UpdateCollection();
+        scrollingObjectCollectionComponent.UpdateContent();
+    }
+
+    public void RemoveLastItem()
+    {
+        int lastItemIndex = gridObjectCollectionComponent.transform.childCount - 1;
+        if (lastItemIndex < 0)
+        {
+            Debug.Log("No items in ScrollObjectCollection");
+            return;
+        }
+
+        var target = gridObjectCollectionComponent.transform.GetChild(lastItemIndex);
+
+        Destroy(target.gameObject);
 
         gridObjectCollectionComponent.UpdateCollection();
         scrollingObjectCollectionComponent.UpdateContent();
